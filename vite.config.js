0622c6662141932fd7b399fa5a5601729f1f5ed1
Franky Hung <arkon.digital.hcf@gmail.com>
2026-05-17 17:27:48 +0800

    added deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import glsl from "vite-plugin-glsl";
 export default {
     root: "src/",
     publicDir: "../public/",
+    base: "/threejs/fractal-glass-gradients/",
     plugins: [
         // Restart server on static/public file change
         restart({ restart: ["../public/**"] }),
